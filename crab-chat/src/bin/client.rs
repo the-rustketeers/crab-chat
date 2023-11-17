6ae8f460ff60fbcc7d2b4aa8b55f066081769dd5
SomeButters <wasdarchaic@gmail.com>
2023-11-16 21:43:17 -0500

    Testing some stuff with threads. (Unoptimal)

--- crab-chat/src/bin/client.rs
+++ crab-chat/src/bin/client.rs
@@ -1,15 +1,35 @@
+// CHANGES MADE FOR TESTING PURPOSES ONLY!
+
 use crab_chat as lib;
-use std::{net::TcpStream, process};
+use json::JsonValue;        // NOTE: This could be just for me because of my IDE, but this is labelled as "Unused"..?
+use std::{net::TcpStream, 
+    process, 
+    time,
+};
 
 fn main() {
-    let connection: TcpStream = TcpStream::connect(lib::ADDRESS).unwrap_or_else(|e| {
+    let mut connection: TcpStream = TcpStream::connect(lib::ADDRESS).unwrap_or_else(|e| {
         eprintln!("Error: {e}.");
         process::exit(1);
     });
-    connection_loop(connection);
+    send_loop(&mut connection);
+    let n_s: String = connection_loop(&mut connection);
+    println!("{}", n_s);
+    std::thread::sleep(time::Duration::from_millis(1000)); // TESTING: Used to test concurrency in sending and receiving despite one concurrent server program.
+    send_loop(&mut connection);
 }
 
-fn connection_loop(mut stream: TcpStream) {
-    let obj = lib::receive_json_packet(&mut stream);
-    println!("{:#?}", obj);
+/// FOR COMMIT: modified to print packet message literal for ease of development (personally helps me only, I understand it isn't practical)
+fn connection_loop(stream: &mut TcpStream) -> String {
+    let obj = lib::receive_json_packet(stream);
+    obj["message"].to_string()
+}
+
+/// send_loop is equivalent to server-side's sending.
+fn send_loop(s: &mut TcpStream) {
+    let name_prompt = json::object! {
+        message: "Test message from client!"
+    };
+    println!("{:#?}", name_prompt.dump());
+    lib::send_json_packet(s, name_prompt);
 }
--- crab-chat/src/bin/server.rs
+++ crab-chat/src/bin/server.rs
@@ -1,8 +1,11 @@
+// CHANGES MADE FOR TESTING PURPOSES ONLY!
+
 use crab_chat as lib;
 use json::object;
 use std::{
     net::{TcpListener, TcpStream},
     process,
+    time,
 };
 
 const ERR: i32 = -1;
@@ -12,6 +15,10 @@ const ERR: i32 = -1;
  *   demonstrates binding, listening, and finally connecting to a socket.
  */
 fn main() {
+
+    let tpool = lib::ThreadPool::new(10); // Size basically means nothing here, but would be halved due to double thread usage.
+                                                           // There is most certainly a better way to handle threads, I'm sure.
+
     //set up TcpListener, bind to port, and listen for connections
     let listener: TcpListener = TcpListener::bind(lib::ADDRESS).unwrap_or_else(|why| {
         eprintln!("ERROR: {why}");
@@ -27,20 +34,49 @@ fn main() {
         match stream {
             Ok(mut s) => {
                 println!("INFO: Got a connection from {:?}", s.peer_addr().unwrap());
-                connection_loop(&mut s);
+                let mut readstream = s.try_clone().expect("Cannot clone stream");
+                tpool.execute(move ||                           // NOTE FOR ADDITION!: move is used for the closure in order to kill stream after use.
+                                                                //                     stream is now OWNED by thread and will terminate after scope is lost. (End of thread execution)
+                    {                                           //                     This is why stream is cloned for read purposes. Both streams identical.
+
+                        connection_loop(&mut s);                // ISSUE: Due to the fact that s is being moved, it will be killed after connection_loop finishes. 
+                                                                //        Because I suspect the server will only ever send messages after receiving them (For pushing)
+                                                                //        This (in theory) should not be a problem. There are solutions to this- should it be one, though. 
+                    }
+                );
+
+                tpool.execute(move || 
+                    {
+                        loop {
+                        let n_s: String = receiver_loop(&mut readstream); // NOTE FOR ADDITION!: prints out values so is readable server-side to prove thread is working properly.
+                        println!("{}", n_s);
+                                                                          // THIS WILL PANIC! After the client's connection is finished sending ALL PACKETS PLANNED!
+                        }                                                 // Because it is a thread, this is fine (enough), but not good. 
+                    }
+                );
+
             }
+
             Err(why) => {
                 eprintln!("ERROR: {why}");
                 process::exit(ERR);
             }
         }
     }
+
+    
 }
 
 fn connection_loop(s: &mut TcpStream) {
     let name_prompt = object! {
-        message: "Please input a nickname: "
+        message: "Test message from server!"
     };
     println!("{:#?}", name_prompt.dump());
     lib::send_json_packet(s, name_prompt);
+}
+
+/// server-side receiving and printing. only prints message for testing purposes.
+fn receiver_loop(s: &mut TcpStream) -> String {
+    let obj = lib::receive_json_packet(s);
+    obj["message"].to_string()
 }
