7f01eb521c04107f4d64934f6da23e17ac322a75
SomeButters <wasdarchaic@gmail.com>
2023-11-24 16:38:44 -0500

    Testing with channels
    Wanted to see if I could easily figure out crossing information between channels in an hour.

--- crab-chat/src/bin/client.rs
+++ crab-chat/src/bin/client.rs
@@ -1,31 +1,49 @@
 // CHANGES MADE FOR TESTING PURPOSES ONLY!
-
 use crab_chat as lib;
-use std::{net::TcpStream, process, time};
+use std::{net::TcpStream, process, time, io, thread,};
 
 fn main() {
+    
     let mut connection: TcpStream = TcpStream::connect(lib::ADDRESS).unwrap_or_else(|e| {
         eprintln!("Error: {e}.");
         process::exit(1);
     });
-    send_loop(&mut connection);
-    let n_s: String = connection_loop(&mut connection);
-    println!("{}", n_s);
-    std::thread::sleep(time::Duration::from_millis(1000)); // TESTING: Used to test concurrency in sending and receiving despite one concurrent server program.
-    send_loop(&mut connection);
+
+    let mut stream_sender = connection.try_clone().unwrap();
+    let mut stream_reader = connection.try_clone().unwrap();
+
+    thread::spawn(move || loop {
+        let n_s: String = rec_loop(&mut stream_reader);
+        println!("{}", n_s);
+    });
+    loop {
+        let mut msg = String::new();
+        
+        io::stdin()
+            .read_line(&mut msg)
+            .expect("Failed to read input");
+
+        let msg = msg.trim();
+
+        if msg == "!!" {
+            println!("[GOODBYE]");
+            break;
+        };
+
+        send_loop(&mut stream_sender, msg.to_string());
+    };
 }
 
 /// FOR COMMIT: modified to print packet message literal for ease of development (personally helps me only, I understand it isn't practical)
-fn connection_loop(stream: &mut TcpStream) -> String {
+fn rec_loop(stream: &mut TcpStream) -> String {
     let obj = lib::receive_json_packet(stream);
     obj["message"].to_string()
 }
 
 /// send_loop is equivalent to server-side's sending.
-fn send_loop(s: &mut TcpStream) {
+fn send_loop(s: &mut TcpStream, msg: String) {
     let name_prompt = json::object! {
-        message: "Test message from client!"
+        message: msg.as_str()
     };
-    println!("{:#?}", name_prompt.dump());
     lib::send_json_packet(s, name_prompt);
 }
--- crab-chat/src/bin/server.rs
+++ crab-chat/src/bin/server.rs
@@ -5,7 +5,9 @@ use json::object;
 use std::{
     net::{TcpListener, TcpStream},
     process,
+    time,
 };
+use crossbeam::{self, channel::at};
 
 const ERR: i32 = -1;
 
@@ -14,6 +16,7 @@ const ERR: i32 = -1;
  *   demonstrates binding, listening, and finally connecting to a socket.
  */
 fn main() {
+
     let tpool = lib::ThreadPool::new(10);
     // Size basically means nothing here, but would be halved due to double thread usage.
     // There is most certainly a better way to handle threads, I'm sure.
@@ -29,30 +32,67 @@ fn main() {
         listener.local_addr().unwrap()
     );
 
+    let (tx, rx) = crossbeam::channel::unbounded();
+    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut s) => {
                 println!("INFO: Got a connection from {:?}", s.peer_addr().unwrap());
                 let mut readstream = s.try_clone().expect("Cannot clone stream");
+                let mut sendstream = s.try_clone().expect("Cannot clone stream");
                 // NOTE FOR ADDITION!: move is used for the closure in order to kill stream after use.
                 // stream is now OWNED by thread and will terminate after scope is lost. (End of thread execution)
                 // This is why stream is cloned for read purposes. Both streams identical.
-                tpool.execute(move || {
-                    connection_loop(&mut s);
-                    // ISSUE: Due to the fact that s is being moved, it will be killed after connection_loop finishes.
-                    // Because I suspect the server will only ever send messages after receiving them (For pushing)
-                    // This (in theory) should not be a problem. There are solutions to this- should it be one, though.
-                });
+
+                let thread_rx = rx.clone();
+                let mut send_string: String;
+                let mut prev_string: String = "".to_string();
 
                 tpool.execute(move || {
+
+                    loop {
+                        std::thread::sleep(time::Duration::from_millis(100));
+                        match thread_rx.try_recv() {
+                            Ok(send_string) => {
+                                if (send_string == prev_string) {
+                                    println!("Same string!");
+                                    continue;
+                                }
+                                if (send_string != prev_string) {
+                                     connection_loop(&mut sendstream, &send_string);
+                                    prev_string = send_string;
+                                }
+                            },
+                            Err(_) => {
+                                continue;
+                            }
+                        }
+
+
+                        //let send_string = thread_rx.recv().unwrap();
+                        //connection_loop(&mut sendstream, &send_string);
+
+                        // ISSUE: Due to the fact that s is being moved, it will be killed after connection_loop finishes.
+                        // Because I suspect the server will only ever send messages after receiving them (For pushing)
+                        // This (in theory) should not be a problem. There are solutions to this- should it be one, though.
+                    }
+                });
+                
+                let thread_tx = tx.clone();
+
+                tpool.execute(move || {
+
                     loop {
                         let n_s: String = receiver_loop(&mut readstream);
                         // NOTE FOR ADDITION!: prints out values so is readable server-side to prove thread is working properly.
                         println!("{}", n_s);
                         // THIS WILL PANIC! After the client's connection is finished sending ALL PACKETS PLANNED!
+                        thread_tx.send(n_s).unwrap();
                     }
                     // Because it is a thread, this is fine (enough), but not good.
                 });
+
             }
 
             Err(why) => {
@@ -63,9 +103,9 @@ fn main() {
     }
 }
 
-fn connection_loop(s: &mut TcpStream) {
+fn connection_loop(s: &mut TcpStream, snd: &String) {
     let name_prompt = object! {
-        message: "Test message from server!"
+        message: snd.as_str()
     };
     println!("{:#?}", name_prompt.dump());
     lib::send_json_packet(s, name_prompt);
