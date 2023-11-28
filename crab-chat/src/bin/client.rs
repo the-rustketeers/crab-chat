4f6f1836b37a80a96a79e830347ebd768e130857
Ediblelnk <peters1731@gmail.com>
2023-11-28 08:54:52 -0500

    linted the file for errors

--- crab-chat/src/bin/client.rs
+++ crab-chat/src/bin/client.rs
@@ -1,10 +1,9 @@
 // CHANGES MADE FOR TESTING PURPOSES ONLY!
 use crab_chat as lib;
-use std::{net::TcpStream, process, time, io, thread,};
+use std::{io, net::TcpStream, process, thread};
 
 fn main() {
-    
-    let mut connection: TcpStream = TcpStream::connect(lib::ADDRESS).unwrap_or_else(|e| {
+    let connection: TcpStream = TcpStream::connect(lib::ADDRESS).unwrap_or_else(|e| {
         eprintln!("Error: {e}.");
         process::exit(1);
     });
@@ -12,14 +11,16 @@ fn main() {
     let mut stream_sender = connection.try_clone().unwrap();
     let mut stream_reader = connection.try_clone().unwrap();
 
-    thread::spawn(move || loop { // Spawns thread to read in received information
+    thread::spawn(move || loop {
+        // Spawns thread to read in received information
         let n_s: String = rec_loop(&mut stream_reader);
         println!("{}", n_s);
     });
 
-    loop { // Loops main to send and get use input
+    loop {
+        // Loops main to send and get use input
         let mut msg = String::new();
-        
+
         io::stdin()
             .read_line(&mut msg)
             .expect("Failed to read input");
@@ -32,7 +33,7 @@ fn main() {
         };
 
         send_loop(&mut stream_sender, msg.to_string());
-    };
+    }
 }
 
 /// FOR COMMIT: modified to print packet message literal for ease of development (personally helps me only, I understand it isn't practical)
--- crab-chat/src/bin/server.rs
+++ crab-chat/src/bin/server.rs
@@ -1,13 +1,12 @@
 // CHANGES MADE FOR TESTING PURPOSES ONLY!
 
 use crab_chat as lib;
+use crossbeam;
 use json::object;
 use std::{
     net::{TcpListener, TcpStream},
-    process,
-    time,
+    process, time,
 };
-use crossbeam::{self, channel::at};
 
 const ERR: i32 = -1;
 
@@ -16,7 +15,6 @@ const ERR: i32 = -1;
  *   demonstrates binding, listening, and finally connecting to a socket.
  */
 fn main() {
-
     let tpool = lib::ThreadPool::new(10);
     // Size basically means nothing here, but would be halved due to double thread usage.
     // There is most certainly a better way to handle threads, I'm sure.
@@ -33,11 +31,10 @@ fn main() {
     );
 
     let (tx, rx) = crossbeam::channel::unbounded();
-    
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut s) => {
+            Ok(s) => {
                 println!("INFO: Got a connection from {:?}", s.peer_addr().unwrap());
                 let mut readstream = s.try_clone().expect("Cannot clone stream");
                 let mut sendstream = s.try_clone().expect("Cannot clone stream");
@@ -45,33 +42,34 @@ fn main() {
                 // stream is now OWNED by thread and will terminate after scope is lost. (End of thread execution)
                 // This is why stream is cloned for read purposes. Both streams identical.
 
-
                 let thread_tx2 = tx.clone();
                 let thread_rx = rx.clone();
-                let mut send_string: String;                    // Two declared strings to be used for the reading.
+                // Two declared strings to be used for the reading.
                 let mut prev_string: String = "".to_string();
 
                 tpool.execute(move || {
-
                     loop {
-                        std::thread::sleep(time::Duration::from_millis(100)); // Loops through indefinitely; an attempt to read from channel across multiple threads.
-                        match thread_rx.recv() {                              // This match case may not be entirely useful yet, I'm not sure.
-                            Ok(send_string) => {                          // Right now the main issue is getting the information from the out-end of the channel
-                                if (send_string == prev_string) {                 // to be read from every thread. The information DOES NOT stay there, and I'm done trying to
-                                    println!("Same string!");                     // figure out how to force it to read it. For now.
+                        std::thread::sleep(time::Duration::from_millis(100));
+                        // Loops through indefinitely; an attempt to read from channel across multiple threads.
+                        match thread_rx.recv() {
+                            // This match case may not be entirely useful yet, I'm not sure.
+                            Ok(send_string) => {
+                                // Right now the main issue is getting the information from the out-end of the channel
+                                if send_string == prev_string {
+                                    // to be read from every thread. The information DOES NOT stay there, and I'm done trying to
+                                    println!("Same string!"); // figure out how to force it to read it. For now.
                                     continue;
                                 }
-                                if (send_string != prev_string) {
+                                if send_string != prev_string {
                                     connection_loop(&mut sendstream, &send_string);
                                     prev_string = send_string.clone();
                                     thread_tx2.send(send_string).unwrap();
                                 }
-                            },
+                            }
                             Err(_) => {
                                 continue;
                             }
                         }
-
 
                         //let send_string = thread_rx.recv().unwrap();
                         //connection_loop(&mut sendstream, &send_string);
@@ -81,11 +79,10 @@ fn main() {
                         // This (in theory) should not be a problem. There are solutions to this- should it be one, though.
                     }
                 });
-                
+
                 let thread_tx = tx.clone(); // Channel clone, for the move in thread
 
                 tpool.execute(move || {
-
                     loop {
                         let n_s: String = receiver_loop(&mut readstream);
                         // NOTE FOR ADDITION!: prints out values so is readable server-side to prove thread is working properly.
@@ -95,7 +92,6 @@ fn main() {
                     }
                     // Because it is a thread, this is fine (enough), but not good.
                 });
-
             }
 
             Err(why) => {
