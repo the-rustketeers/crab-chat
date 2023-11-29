4fa9dd1feeb221ce849e2407af1a1a4bf8175a8d
Ediblelnk <peters1731@gmail.com>
2023-11-29 13:25:40 -0500

    Basic sending and connecting

--- crab-chat/src/bin/server.rs
+++ crab-chat/src/bin/server.rs
@@ -1,12 +1,17 @@
 use crab_chat as lib;
+use json::JsonValue;
 
-use std::{net::TcpListener, process, thread};
+use std::{
+    net::{TcpListener, TcpStream},
+    process,
+    sync::mpsc::{self, Receiver, TryRecvError},
+    thread,
+};
 
 const ERR: i32 = -1;
 
 /**
- * Description: main program. takes exactly 1 command line argument.
- *   demonstrates binding, listening, and finally connecting to a socket.
+ * Main program. Hosts the Chat Server
  */
 fn main() {
     //set up TcpListener, bind to port, and listen for connections
@@ -15,23 +20,130 @@ fn main() {
         process::exit(ERR);
     });
 
+    // Info message
     println!(
-        "INFO: Server started on and listening for connections on address {:?}",
+        "[SERVER STARTED AND LISTENING FOR CONNECTION ON {:?}]",
         listener.local_addr().unwrap()
     );
 
+    // Set up mpsc channels to send to thread that handles pushing messages
+    let (json_producer, json_consumer) = mpsc::channel::<JsonValue>();
+    let (stream_producer, stream_consumer) = mpsc::channel::<TcpStream>();
+
+    // Spawns new thread that handles fetching messages and pushing messages
+    let fetcher = thread::spawn(move || {
+        fetch_loop(json_consumer, stream_consumer);
+    });
+
+    // Main thread handles setting up new connections
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("[CONNECTION FROM {:?}]", stream.peer_addr().unwrap());
-                thread::spawn(|| {
-                    lib::connection_loop(stream);
+                // log who connected to the server
+                println!(
+                    "[{:?} CONNECTED TO THE SERVER]",
+                    stream.peer_addr().unwrap()
+                );
+
+                // Clone the producer, so it can forward messages to the fetcher
+                let p = json_producer.clone();
+                // Clone the stream, so the fetcher can send messages to it
+                stream_producer.send(stream.try_clone().unwrap()).unwrap();
+
+                // Spawn a new thread that handles listening to this connection
+                thread::spawn(move || {
+                    connection_loop(stream, p);
                 });
             }
+            Err(why) => eprintln!("[ERROR: {why}]"),
+        }
+    }
+
+    // Ideally should wait until the fetcher thread ends before closing program.
+    // Currently this place of code is never reached because the main thread
+    // continuously just looks for possible connections
+    fetcher.join().unwrap();
+}
+
+/**
+ * This function takes a TcpStream and Producer part of a channel
+ * and will forward any messages it receives through the channel
+ */
+fn connection_loop(mut listener: TcpStream, json_producer: mpsc::Sender<JsonValue>) {
+    loop {
+        let obj = match lib::receive_json_packet(&mut listener) {
+            Ok(obj) => obj,
+            Err(lib::JsonError::ConnectionAborted) => break,
+        };
+
+        lib::log_json_packet(&obj);
+
+        if obj["kind"] == "disconnection" {
+            println!(
+                "[{:?} DISCONNECTED FROM THE SERVER]",
+                listener.peer_addr().unwrap()
+            );
+            break;
+        }
+
+        match json_producer.send(obj) {
+            Ok(()) => (),
             Err(why) => {
                 eprintln!("ERROR: {why}");
-                process::exit(ERR);
+                break;
             }
+        };
+    }
+}
+
+/**
+ * Takes any messages that the json consumer gets and forwards it to all client
+ * streams currently seen as connected
+ */
+fn fetch_loop(json_consumer: Receiver<JsonValue>, stream_consumer: Receiver<TcpStream>) {
+    // This holds all of the client streams to try to send to
+    // As more clients connect this grows and shrinks
+    let mut client_list: Vec<TcpStream> = vec![];
+
+    // this loop handles probing for new client streams, new json packets sent
+    loop {
+        // see if a client stream is available, if so add it to list
+        match stream_consumer.try_recv() {
+            Ok(stream) => client_list.push(stream),
+            Err(why) => match why {
+                TryRecvError::Disconnected => {
+                    eprint!("[FATAL ERROR: {why}]");
+                    break;
+                }
+                TryRecvError::Empty => (),
+            },
+        };
+
+        // see if a json packet is available, if so forward it to clients
+        match json_consumer.try_recv() {
+            Ok(obj) => push_to_clients(&mut client_list, obj),
+            Err(why) => match why {
+                TryRecvError::Disconnected => {
+                    eprint!("[FATAL ERROR: {why}]");
+                    break;
+                }
+                TryRecvError::Empty => (),
+            },
+        };
+    }
+}
+
+/**
+ * This function just loops through all clients in the client list and attempts
+ * to send a json packet to them.
+ */
+fn push_to_clients(client_list: &mut Vec<TcpStream>, obj: JsonValue) {
+    for i in 0..client_list.len() {
+        match lib::send_json_packet(&mut client_list[i], obj.clone()) {
+            Ok(()) => (),
+            // need to implement this,
+            // vvv just a little more complicated that I thought
+            Err(_) => (),
         }
     }
 }
--- crab-chat/src/lib.rs
+++ crab-chat/src/lib.rs
@@ -1,12 +1,11 @@
-use json::{object, JsonValue};
+use json::JsonValue;
 use std::{
-    io::{self, Read, Write},
+    io::{Read, Write},
     net::TcpStream,
-    str, thread,
 };
 
 pub const ADDRESS: &str = "127.0.0.1:13579";
-const EXIT_CODE: &str = "!!";
+pub const EXIT_CODE: &str = "!!";
 
 /**
  * Sends a json object through the TcpStream, by first creating the object, sending its size, and then the object.
@@ -48,63 +47,17 @@ pub fn receive_json_packet(s: &mut TcpStream) -> Result<JsonValue, JsonError> {
         Err(_) => return Err(JsonError::ConnectionAborted),
         Ok(_) => (),
     }
-    let packet: &str = str::from_utf8(&packet_buf).unwrap();
+    let packet: &str = std::str::from_utf8(&packet_buf).unwrap();
     // println!("Received Packet: {}", packet);
 
     Ok(json::parse(packet).unwrap())
 }
 
+#[derive(Debug)]
 pub enum JsonError {
     ConnectionAborted,
 }
 
-pub fn connection_loop(stream: TcpStream) {
-    println!("[START TYPING AND HIT ENTER TO SEND A MESSAGE]");
-    let addr = stream.local_addr().unwrap();
-
-    // copy stream and push it into a thread to handle getting input from user
-    let mut stream_reader = stream.try_clone().unwrap();
-    let reader = thread::spawn(move || loop {
-        let mut msg = String::new();
-
-        // read input from the user
-        io::stdin()
-            .read_line(&mut msg)
-            .expect("Failed to read line");
-
-        let msg = msg.trim();
-
-        // check if input is to exit, and leave the thread if so
-        if msg == EXIT_CODE {
-            println!("[GOODBYE]");
-            break;
-        };
-
-        // wrap message in a json object
-        let obj = object! {
-            author: format!("{addr}"),
-            message: msg,
-        };
-
-        // try to send json object, fails if server/client exited already
-        match send_json_packet(&mut stream_reader, obj) {
-            Ok(()) => (),
-            Err(_) => break,
-        };
-    });
-
-    // copy stream and move it into a thread to handle receiving incoming messages from the server
-    let mut stream_listener = stream.try_clone().unwrap();
-    let _listener = thread::spawn(move || loop {
-        // endlessly try to get/print the next json packet from stream
-        // fails if server/client exited already
-        let obj = match receive_json_packet(&mut stream_listener) {
-            Ok(obj) => obj,
-            Err(_) => break,
-        };
-        println!("{} says: \"{}\"", obj["author"], obj["message"]);
-    });
-
-    // hold until the input reader ends
-    reader.join().unwrap();
+pub fn log_json_packet(obj: &JsonValue) {
+    println!("{:?}", obj);
 }
